f311be9bc4c65e17923c7567964c4a28cc1b752e
Kathireshan Manoharan <kathireshan258@gmail.com>
2022-04-15 17:34:24 +0530

    Working with objects

--- javascript/objects-05-dynamic-access-and-assignment/assets/scripts/objects.js
+++ javascript/objects-05-dynamic-access-and-assignment/assets/scripts/objects.js
@@ -1,31 +1,62 @@
-const movieList = document.getElementById('movie-list');
+const addMovieBtn  = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+const movies = [];
 
-movieList.style['background-color'] = 'red';
-movieList.style.display = 'block';
+const  renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list');
+  if (movies.length <= 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
 
-const userChosenKeyName = 'level';
+  const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
 
-let person = {
-  'first name': 'Max',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  [userChosenKeyName]: '...',
-  greet: function() {
-    alert('Hi there!');
-  },
-  1.5: 'hello'
-};
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    const {info: movieInfo, ...otherProperty} = movie;
+    // let text = movieInfo.title + ' - ';
+    let text = movie.getFormattedTitle() + ' - ';
+    for (const key in movieInfo) {
+      if (key !== 'title') {
+        text = text + `${key}: ${movieInfo[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.insertAdjacentElement('afterbegin', movieEl);
+  })
+}
 
-// ...
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value.trim();
+  const extraName = document.getElementById('extra-name').value.trim();
+  const extraValue = document.getElementById('extra-value').value.trim();
 
-// person.age = 31;
-delete person.age;
-// person.age = undefined;
-// person.age = null;
-person.isAdmin = true;
+  if (title === '' || extraName === '' || extraValue === '') {
+    return;
+  }
 
-const keyName = 'first name';
+  const newMovie = {
+    info: {
+        title,
+        [extraName] : extraValue
+    },
+    id: Math.random(),
+    getFormattedTitle() { // this can be written as getFormattedTitle: functio() {}
+      return this.info.title.toUpperCase();
+    }
+  };
+  movies.push(newMovie);
+  console.log(movies);
+  renderMovies();
+}
 
-console.log(person[keyName]);
-console.log(person[1.5]);
-console.log(person);
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+}
+
+searchBtn.addEventListener('click', searchMovieHandler);
+addMovieBtn.addEventListener('click', addMovieHandler);

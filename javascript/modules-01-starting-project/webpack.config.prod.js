621819adc633e12ecd44e2d085e8f22e93a71ae6
Kathireshan Manoharan <kathireshan258@gmail.com>
2022-05-09 16:46:04 +0530

    adding Webpack bundler and dev server

---
+++ javascript/modules-01-starting-project/webpack.config.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+        filename: '[contenthash].js',
+        path: path.resolve(__dirname, 'assets', 'scripts'),
+        publicPath: 'assets/scripts/'
+    },
+    devServer: {
+        static: './'
+    },
+    // plugins: [new webpack.EvalSourceMapDevToolPlugin({})]
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+          options: {
+            // devTools: 'eval-source-map'
+            // devTools: 'eval-cheap-module-source-map'
+            // devTools: 'cheap-module-source-map'
+            devTools: 'source-map'
+            // devTools: 'cheap-source-map'
+          }
+        }),
+        new CleanPlugin.CleanWebpackPlugin()
+      ]
+};
